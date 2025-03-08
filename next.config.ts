cb987542d72b9c959bf28e31159e0d6eae7297d3
omkarghodekar140803@gmail.com <omkarghodekar140803@gmail.com>
2025-03-08 19:07:08 +0530

    images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["i.postimg.cc"],
+    domains: ["images.unsplash.com", "i.postimg.cc"],
   },
 };
 
